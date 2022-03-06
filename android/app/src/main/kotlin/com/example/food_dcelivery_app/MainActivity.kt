6b1462d64ec174c4d58d3999bce5c1f7413b03e1
milan Katira <milankatira26@gmail.com>
2022-03-07 00:22:13 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/food_dcelivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_dcelivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/icon_and_text_widgets.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:food_dcelivery_app/widgets/small_text.dart';
+
+class IconAndTextWidgets extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconColor;
+
+  const IconAndTextWidgets({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.iconColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: iconColor,
+        ),
+        SizedBox(width: 5),
+        SmallText(text: text),
+      ],
+    );
+  }
+}
